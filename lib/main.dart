245c43fbfca66c38f630b2e6abcd73559c62bc93
panth <panth@local.dev>
2026-06-02 01:55:24 +0900

    fix: rollback font family to default Pretendard and clean up style

--- lib/main.dart
+++ lib/main.dart
@@ -1441,11 +1441,7 @@ class _Chapter2PuzzleQ2ScreenState extends State<Chapter2PuzzleQ2Screen> {
         elevation: 0,
         title: const Text(
           '미션! 수학체험센터의 반짝별을 찾아서',
-          style: TextStyle(
-            fontWeight: FontWeight.w800,
-            fontSize: 24,
-            fontFamily: 'GangwonEduAll',
-          ),
+          style: TextStyle(fontWeight: FontWeight.w800, fontSize: 24),
         ),
         centerTitle: true,
         actions: [
@@ -1497,7 +1493,6 @@ class _Chapter2PuzzleQ2ScreenState extends State<Chapter2PuzzleQ2Screen> {
                         fontSize: 22,
                         fontWeight: FontWeight.w800,
                         color: Color(0xFF163988),
-                        fontFamily: 'GangwonEduAll',
                       ),
                       textAlign: TextAlign.center,
                     ),
@@ -1631,7 +1626,6 @@ class _Chapter2PuzzleQ2ScreenState extends State<Chapter2PuzzleQ2Screen> {
                                                         color: Color(
                                                           0xFF7A5A00,
                                                         ),
-                                                        fontFamily: 'GangwonEduAll',
                                                       ),
                                                     ),
                                                     const SizedBox(
@@ -1675,7 +1669,6 @@ class _Chapter2PuzzleQ2ScreenState extends State<Chapter2PuzzleQ2Screen> {
                                                   '조각 보관함',
                                                   style: TextStyle(
                                                     fontSize: 18,
-                                                    fontFamily: 'GangwonEduAll',
                                                     fontWeight: FontWeight.w900,
                                                     color: Color(0xFF355AA8),
                                                   ),
@@ -1826,7 +1819,7 @@ class _Chapter2PuzzleQ2ScreenState extends State<Chapter2PuzzleQ2Screen> {
                               fontSize: 20,
                               fontWeight: FontWeight.w800,
                               color: Color(0xFF355AA8),
-                              fontFamily: 'GangwonEduAll',
+
                             ),
                           ),
                         ),
@@ -1852,7 +1845,7 @@ class _Chapter2PuzzleQ2ScreenState extends State<Chapter2PuzzleQ2Screen> {
                               fontSize: 20,
                               fontWeight: FontWeight.w800,
                               color: Color(0xFF355AA8),
-                              fontFamily: 'GangwonEduAll',
+
                             ),
                           ),
                         ),
@@ -1874,7 +1867,7 @@ class _Chapter2PuzzleQ2ScreenState extends State<Chapter2PuzzleQ2Screen> {
                             style: TextStyle(
                               fontSize: 20,
                               fontWeight: FontWeight.w900,
-                              fontFamily: 'GangwonEduAll',
+
                             ),
                           ),
                         ),
